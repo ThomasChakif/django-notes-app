ac8f3f65b7845a7224ecd171300a93e492756157
thc225 <thc225@lehigh.edu>
2025-06-03 10:17:44 -0400

    begin frontend work, set up form functionality

---
+++ frontend/src/App.jsx
@@ -0,0 +1,45 @@
+import react from 'react'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import Logout from './pages/Logout'
+
+function Logout() {
+  localStorage.clear() //clear access and refresh tokens
+  return <Navigate to="/login"/>
+}
+
+function RegisterAndLogout() {
+  localStorage.clear() //when registering, we clear to make sure we dont have any old tokens lingering still
+  return <Register />
+}
+
+
+
+function App() {
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          // only allow Home within protected route
+          <ProtectedRoute>
+            <Home /> 
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/login" element={<Login />}/>
+      <Route path="/logout" element={<Logout />}/>
+      <Route path="/register" element={<Register />}/>
+      {/* not found is like a 404 page */}
+      <Route path="*" element={<NotFound />}/> 
+    </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,54 @@
+import {useState} from 'react'
+import api from '../api'
+import { useNavigate } from 'react-router-dom'
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants'
+
+
+function Form({route, method}) {
+    const[username, setUsername] = useState("")
+    const[password, setPassword] = useState("")
+    const[loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+
+    {/* if the method is login, text will display Login. Else, display register */}
+    const name = method ==="login" ? "Login" : "Register"
+
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefault() //will remove default behavior so we dont reload page
+
+        try{
+            const res = await api.post(route, {username, password})
+            if(method === "login") { //if the user attempted to login, we set their access and refresh tokens
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+                navigate('/')
+            }else{ //else if they tried to register, redirect to login page so that we can get their tokens
+                navigate("/login")
+            }
+        }catch(err){
+            alert(err)
+        }finally{ //whether it works or not, we still want to turn the loading indicator off
+            setLoading(false)
+        }
+    }
+
+    return <form className = "form-container" onSubmit={handleSubmit}>
+        <h1>{name}</h1> 
+        <input 
+            className="form-input" 
+            type="text" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+            placeholder="Username">
+        </input>
+        <input 
+            className="form-input" 
+            type="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            placeholder="Password">
+        </input>
+        <button className='form-button' type='submit'>{name}</button>
+    </form>
+}
