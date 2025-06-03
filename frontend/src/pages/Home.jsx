3a4cc58271847a1243b146d9bd620b4f66339d5a
thc225 <thc225@lehigh.edu>
2025-06-03 11:37:22 -0400

    note functionality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,8 +23,8 @@ function Home() {
         api.delete(`/api/notes/delete/${id}/`).then((res) => {
             if (res.status === 204) alert('Note was deleted!')
             else alert("Failed to delete note.")
+            getNotes();
         }).catch((err) => alert(err))
-        getNotes();
     };
 
     //create a new note
@@ -33,11 +33,41 @@ function Home() {
         api.post("/api/notes/", {content, title}).then((res) => {
             if (res.status === 201) alert("Note created")
             else alert("Failed to create note")
+            getNotes();
         }).catch((err) => alert(err))
-        getNotes();
     };
 
-    return <div>Home</div>
+    return (
+    <div>
+        <div>
+            <h2>Notes</h2>
+        </div>
+        <h2>Create a Note</h2>
+        <form onSubmit={createNote}>
+            <label htmlFor="title">Title:</label>
+            <br/>
+            <input 
+                type="text" 
+                id="title" 
+                name="title" 
+                required 
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <label htmlFor="content">Content:</label>
+            <br/>
+            <textarea 
+                id="content" 
+                name="content" 
+                required 
+                value={content} 
+                onChange={(e) => setContent(e.target.value)}
+            />
+            <br/>
+            <input type="submit" value = 'Submit'></input>
+        </form>
+    </div>
+    )
 }
 
 export default Home
